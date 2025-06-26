70f8efb1ebfec24b977802affdbdc709ec98cad3
Mohd Saad <mohdsaad786@gmail.com>
2025-06-26 12:48:35 +0530

    Allowed render host in vite.config.js

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,16 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-    },
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
-});
+  plugins: [react()],
+  preview: {
+    port: 4173,
+    host: true,
+    allowedHosts: ['saas-product-dashboard.onrender.com']  // 👈 ye line add karo
+  }
+})
